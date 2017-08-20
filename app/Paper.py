c668c8ac1b1dbdc1a0a61510b71f795f1d603a41
Lucas Romao <lucas.romao.silva@gmail.com>
2017-08-20 08:44:11 -0300

    tirando metodo mock

--- app/Paper.py
+++ app/Paper.py
@@ -41,16 +41,6 @@ class Paper:
         self.link = link
         self.published = published
 
-def getMockPapers():
-    MockPapers = []
-    tags = set(['graph theory'])
-    MockPapers.append(Paper("New discovery in Graph theory", "Grahl Phillip Theodore Ryan", "ect to second tier occasional objects, but most newspapers Watterson's stocking; the Noodle American middle-class and shallowings just commonly birthday celebrate this,' says he have quited for full, third, and the Lacanian Real, with commentary and ice craft of medication' accompanied by Cartoon strips Few editors appear This half of a new present time you were happen against walls, demand rambunction The Duplicator can be stopping Calvin's serve to ask for, often she supposed to fit this whole life\n" \
-   "Watterson had comic books, public decides to market the strip, I wasn't again theories of dollars person Calvin's creation, the open for press refuses together with howling, purposed a unique Easter egg instructor in the Complete, and greated at an 'anatomicalled Calvin, echoing Watterson and the childhood animation spell anything certain things'\n"\
-   "Remaining dinosaurs In a notabloid and civilized Her relations and the Transrelation,' and camping me", "http://www.link.com", "22/02/2017"))
-    MockPapers.append(Paper("Another Interesting Paper", "Joseph Rabbit", "This is a quite interesting abstract about my paper, you should really check it out", "http://www.google.com.br", "22/02/2016"))
-    MockPapers.append(Paper("My Hackathon Paper", "Anthony John Lucian", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", "http://www.facebook.com", "22/02/2015"))
-    return MockPapers
-
 ##TO-DO: add date checking
 ##TO-DO: receive link
 def getUserPapers(user):
@@ -68,6 +58,6 @@ def getPapersForView():
     rawinfo = aac.retrievePaperInfo(set(["Graph Theory"]))
     for meat in rawinfo:
         meat = meat['entry']
-        paper = Paper(clipText(meat['title'], 50), meat['author'], clipText(meat['summary'], 1500), meat['id'], dateFormating(meat['published']))
+        paper = Paper(clipText(meat['title'], 100), meat['author'], clipText(meat['summary'], 1500), meat['id'], dateFormating(meat['published']))
         papers.append(paper)
     return papers
